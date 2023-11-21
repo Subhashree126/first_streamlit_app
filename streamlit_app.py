0eef0269322317d2e2fc7c0998dd01053091918c
Subhashree126 <101786841+Subhashree126@users.noreply.github.com>
2023-11-21 17:12:04 +0530

    Update streamlit_app.py
    changed the breakfast header
    added some more options on breakfast
    added the smoothie header

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
 streamlit.title("My parents new healthy diner")
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 and blueberry oatmeal')
-streamlit.text('Kale,spinach and Rocket Smoothie')
-streamlit.text('Hard-boiled Free-Range egg')
+streamlit.header('Breakfast Favourites')
+streamlit.text('🥣 Omega 3 and blueberry oatmeal')
+streamlit.text('🥗 Kale,spinach and Rocket Smoothie')
+streamlit.text('🐔 Hard-boiled Free-Range egg')
+streamlit.text('🥑🍞Avocado toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
